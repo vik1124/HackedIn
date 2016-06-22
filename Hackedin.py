f16c1708802d8762f4bd424c1fa26d428b72ab64
vikwiz <vik1124@gmail.com>
2016-06-21 22:27:04 -0400

    changed the code to a oop version to aid in creating multiple drivers, also updated code to use current css elements

--- Hackedin.py
+++ Hackedin.py
@@ -1,4 +1,4 @@
-__author__ = 'harithan'
+__author__ = 'vikram modifying harithans code'
 from time import sleep
 from random import randint
 from selenium import webdriver
@@ -6,58 +6,62 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
-user = 'your user name here'
-passw = 'your password here'
-driver = webdriver.Firefox()
-wait = WebDriverWait(driver, 10)
-driver.get('https://www.linkedin.com')
-username = driver.find_element_by_id('session_key-login')
-password = driver.find_element_by_id('session_password-login')
-submit_button = driver.find_element_by_id('login')
-username.send_keys(user)
-password.send_keys(passw)
-submit_button.submit()
-driver1 = webdriver.Firefox()
+user = 'username here'
+passw = 'password here'
 
-driver1.get('https://www.linkedin.com')
-username1 = driver1.find_element_by_id('session_key-login')
-password1 = driver1.find_element_by_id('session_password-login')
-submit_button1 = driver1.find_element_by_id('login')
-username1.send_keys(user)
-password1.send_keys(passw)
-submit_button1.submit()
-#modify the below link based on area and search criteria
-driver.get('https://www.linkedin.com/vsearch/p?keywords=IT%20Recruiter&title=IT%20Recruiter&titleScope=CP&trk=tyah&trkInfo=tarId%3A1418423381547,tas%3Ait%20recruiter,idx%3A1-1-1&rsid=1766873661418423384066&openFacets=N,G,CC&orig=FCTD&f_G=us%3A70')
-WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.ID, 'results_count')))
-soup = BeautifulSoup(driver.page_source, 'xml')
-variable = soup.find(attrs={'class': 'title'})['href']
-# This loop will run as long as linkedin suggests people matching the keywords and profile.
-b = 1
+
+class Automaton:
+
+    def __init__(self):
+        self.links = []
+        self.driver = webdriver.Firefox()
+        self.wait = WebDriverWait(self.driver, 10)
+        self.driver.get('https://www.linkedin.com')
+        self.username = self.driver.find_element_by_id('login-email')
+        self.password = self.driver.find_element_by_id('login-password')
+        self.submit_button = self.driver.find_element_by_name('submit')
+        self.username.send_keys(user)
+        self.password.send_keys(passw)
+        self.submit_button.submit()
+
+    def getFirstSearchLink(self, searchString):
+        self.searchStr = searchString.replace(' ', '+')
+        self.driver.get('https://www.linkedin.com/vsearch/f?type=all&keywords='+ self.searchStr + '&orig=GLHD&rsid=548370671466554148033&pageKey=voltron_federated_search_internal_jsp&trkInfo=&search=Search')
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.ID, 'results_count')))
+        self.soup = BeautifulSoup(self.driver.page_source, 'lxml')
+        self.link = self.soup.find(attrs={'class': 'title main-headline'})['href']
+        return self.link
+
+    def loopThroughLinks(self):
+        self.driver.get(self.link)
+        WebDriverWait(self.driver, 15).until(ec.presence_of_element_located((By.ID, 'control_gen_4')))
+        self.soup = BeautifulSoup(self.driver.page_source, 'xml')
+        try:
+            self.ul = self.soup.find(attrs={'class': 'browse-map-list'})
+            self.a = self.ul.findAll('a')
+            z = randint(0,7)*2 + 1
+            self.links.append(self.a[z]['href'])
+            print self.links[-1]
+            self.link = self.links[-1]
+        except AttributeError:
+            self.link = self.links[-2]
+            del self.links[-1]
+            print'Oops blacklisted by linkedIn if this message repeats more than twice'
+        except IndexError:
+            pass
+        except :
+            self.link = self.links[-2]
+            print'Oops blacklisted by linkedIn if this message repeats more than twice'
+
 x = 1
+drv1 = Automaton()
+drv2 = Automaton()
+print drv1.getFirstSearchLink("software recruiter NYC")
+print drv2.getFirstSearchLink("software recruiter San Diego")
 while 1:
-    print x
-
-    if b:
-        b = 0
-        driver.get(variable)
-        #sleep(randint(1, 4))
-        WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.ID, 'control_gen_4')))
-        soup = BeautifulSoup(driver.page_source, 'xml')
-        try:
-            future = soup.find(attrs={'class': 'recommendation-link'})['href']
-            x += 1
-            variable = future
-        except TypeError:
-            print'Oops blacklisted by linkedIn if this message repeats more than twice'
-    else:
-        b = 1
-        driver1.get(variable)
-        #sleep(randint(1, 4))
-        WebDriverWait(driver1, 10).until(ec.presence_of_element_located((By.ID, 'control_gen_4')))
-        soup = BeautifulSoup(driver1.page_source, 'xml')
-        try:
-            future = soup.find(attrs={'class': 'recommendation-link'})['href']
-            x += 1
-            variable = future
-        except TypeError:
-            print'Oops blacklisted by linkedIn if this message repeats more than twice'
+    print x, ": 1"
+    x += 1
+    drv1.loopThroughLinks()
+    x += 1
+    print x, ": 2"
+    drv2.loopThroughLinks()
